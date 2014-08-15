eee6c561448b56f3dfc34188689a3b0a69e29213
scientific-coder <scientific-coder@bernard-hugueney.org>
2014-08-15 04:30:56 +0200

    initial commit

---
+++ config.clj
@@ -0,0 +1,31 @@
+[:site-title "Santa Rosa Day Care"
+ :site-description "Celia Sherman's Day Care at Berkeley"
+ :site-url "http://www.thousandoakschildcare.com"
+ :in-dir "resources/"
+ :out-dir "html/"
+ :default-template "default.clj"
+ :encoding "UTF-8"
+ :blog-as-index false
+ :create-archives false
+ :atomic-build true
+ :emacs "/usr/bin/emacs"
+ :emacs-eval
+ [
+  ;; '(add-to-list 'load-path "~/source/emacs/ext/org-mode/lisp/")
+  ;;             '(add-to-list 'load-path "~/source/emacs/ext/org-mode/contrib/lisp/")
+  ;;             '(add-to-list 'load-path "~/.emacs.d/elpa/clojure-mode-2.1.1/")
+  ;;             '(require 'htmlize)
+  ;;             '(require 'org)
+  ;;             '(require 'org-macs)
+  ;;             '(require 'ox-html)
+  ;;             '(require 'ob)
+  ;;             '(global-font-lock-mode 1)
+  ;;             '(require 'clojure-mode)
+  ;;             '(setq org-export-with-section-numbers nil)
+  ;;             '(set-face-foreground 'font-lock-string-face "#afafff")
+  ;;             '(set-face-foreground 'font-lock-keyword-face "#ff5f00")
+  ;;             '(set-face-foreground 'font-lock-function-name-face "#d7af00")
+  ;;             '(set-face-foreground 'font-lock-builtin-face "#afd700")
+  ;;             '(set-face-foreground 'font-lock-comment-face "#008787")
+              ]
+ ]
